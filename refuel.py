6a4917081aad0e30096112cafd8b352e90b1b2e2
Prash1723 <s.praveen795@gmail.com>
2023-06-28 00:20:12 +0530

    Refueling script

--- refuel.py
+++ refuel.py
@@ -10,3 +10,11 @@ def refuel(distance: "Distance to be covered", initial_fuel: "Initial fuel in th
     tank_capacity = 5.3 # Litres
     mileage = 44 # kmpl
     fuel_spent = 0
+    distance_covered = 0
+    
+    signal_dict = {1.2: "Low fuel", 0.6: "Very low fuel"}
+
+    while distance_covered < distance:
+        for k, v in signal_dict.items():
+            if fuel_spent == k:
+                rc.log("Fuel capacity : {:.2f} | {:.2f}".format(k,v))
