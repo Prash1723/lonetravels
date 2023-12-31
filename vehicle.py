dd76612adbe506300426edb5768fe0e4a7547b01
Prash1723 <s.praveen795@gmail.com>
2023-12-31 22:55:53 +0530

    import

--- vehicle.py
+++ vehicle.py
@@ -1,16 +1,17 @@
 from rich.console import Console
+import numpy as np
 
 rc = Console()
 
 Class Vehicles():
-	"""All vehicle details"""
+    """All vehicle details"""
 	
-	def Honda_Dio():
-		fuel_capacity = 5.3
-		mileage = 44
-		optimum_speed = np.range(35,46,1)
+    def Honda_Dio():
+	fuel_capacity = 5.3
+    	mileage = 44
+    	optimum_speed = np.range(35,46,1)
 
-	def Honda_Activa():
-		fuel_capacity = 5.3
-		mileage = 40
-		optimum_speed = np.range(35,46,1)
+    def Honda_Activa():
+    	fuel_capacity = 5.3
+	mileage = 40
+    	optimum_speed = np.range(35,46,1)
