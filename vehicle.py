8d073806773a9f0eb8bc49202cc9058af1e16d04
Prash1723 <s.praveen795@gmail.com>
2023-12-31 22:42:53 +0530

    vehicle details

---
+++ vehicle.py
@@ -0,0 +1,16 @@
+from rich.console import Console
+
+rc = Console()
+
+Class Vehicles():
+	"""All vehicle details"""
+	
+	def Honda_Dio():
+		fuel_capacity = 5.3
+		mileage = 44
+		optimum_speed = np.range(35,46,1)
+
+	def Honda_Activa():
+		fuel_capacity = 5.3
+		mileage = 40
+		optimum_speed = np.range(35,46,1)
