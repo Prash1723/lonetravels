2a6aa2df9b94db133d72e581928f4e0eb847447e
Prash1723 <s.praveen795@gmail.com>
2024-01-15 21:57:54 +0530

    adding attributes

--- vehicle.py
+++ vehicle.py
@@ -5,31 +5,47 @@ rc = Console()
 
 class Vehicles():
     """All vehicle details"""
-	
-    def Honda_Dio():
+    self.speed = speed
+    self.mv_time = mv_time
+    self.fuel_capactiy = fuel_capacity
+    self.mileage = mileage
+    self.optimum_speed = optimum_speed
+
+    def Honda_Dio(self):
 	fuel_capacity = 5.3
     	mileage = 44
     	optimum_speed = np.range(35,46,1)
+        speed = 0
 
-    def Honda_Activa():
+    def Honda_Activa(self):
     	fuel_capacity = 5.3
 	mileage = 40
     	optimum_speed = np.range(35,46,1)
+        speed = 0
+
+    def travelling(self):
+        in_cap = self.Honda_Dio
+        if in_cap['speed'] = 
 
 class Camera():
     """Camera details"""
+    self.on_time = on_time
+    self.battery = battery
+    self.FHD = FHD
+    self.HD = HD
+    self.VGA = VGA
 
-    def Mirrorless_cam():
+    def Mirrorless_cam(self):
         FHD = 35.64             # minutes
         HD = 119.27             # minutes
         VGA = 9999              # minutes
         battery = 3             # hours
 
-    def start_camera():
+    def start_camera(self):
         rc.log(battery, style="green")
 
 class location():
     """Location details"""
 
-    def place():
-
+    def place(self):
+        
